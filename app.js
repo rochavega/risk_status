dcbd08d686fdc417cf423ba84ebf10c069972aae
rochavega <cristianogoncalves.vega@gmail.com>
2025-07-14 16:14:04 -0300

    Add files via upload
    versão atualizada

--- app.js
+++ app.js
@@ -10,9 +10,86 @@ async function calculateRisk() {
     const featureServiceUrl = "https://services3.arcgis.com/v7iE2qiYIiE1joi6/arcgis/rest/services/Grid_20x20_Deforest_Final_pol_status/FeatureServer/0/query";
     const whereClause = gridIds.map(id => `id_grid2='${id}'`).join(" OR ");
 
+    var s_3000 = 3000;
+    var s_1000 = 1000;
+    var s_300 = 300;
+    var cdf = 0.0
+    var total = 0;
+    var calc = false;
+
+    function processOneData(data) {
+        data.features.forEach(feature => {
+                    var sample = Math.floor(s_3000 / feature.attributes.field_si_1)
+                    if(feature.attributes.Eligible == 0){
+                        total = 1
+                        return
+                    }
+                    else if(feature.attributes.def_1 == 0){
+                        total = 0
+                        return
+                    }
+                    else if (sample > feature.attributes.Total_1) {
+                        sample = s_1000 / feature.attributes.field_si_1;
+
+                        if (sample > feature.attributes.Total_1) {
+                            sample = s_300 / feature.attributes.field_si_1;
+
+                            if (sample > feature.attributes.Total_1) {
+                                sample = s_3000 / feature.attributes.UCSBACRES;
+
+                                if (sample > feature.attributes.Total_1) {
+                                    sample = s_1000 / feature.attributes.UCSBACRES;
+
+                                    if (sample > feature.attributes.Total_1) {
+                                        sample = s_300 / feature.attributes.UCSBACRES;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
+                    //const cdf = jStat.hypgeom.cdf(k, N, K, n);
+                    cdf = 1 - (jStat.hypgeom.cdf(0, feature.attributes.Total_1, feature.attributes.def_1, parseInt(sample)));
+                    if (cdf !== null && !isNaN(cdf)) {
+                        total = cdf
+                    }
+                })};
+    
+    function processManyData(data) {
+        var deforst_sum = 0;
+        var total_fields_sum = 0;
+        var field_size_sum = 0;
+        var size_mean = 0;
+
+        data.features.forEach(feature => {
+            deforst_sum += feature.attributes.def_1;
+            total_fields_sum += feature.attributes.Total_1;
+            field_size_sum += feature.attributes.field_si_1;
+        });
+            console.log(field_size_sum)
+            size_mean = field_size_sum / data.features.length
+            console.log(total_fields_sum)
+            console.log(deforst_sum)
+            console.log(size_mean)
+            var sample = Math.floor(s_3000 / size_mean)
+            console.log(sample)
+
+            cdf = 1 - (jStat.hypgeom.cdf(0, total_fields_sum, deforst_sum, parseInt(sample)));
+            console.log(cdf)
+            if (cdf !== null && !isNaN(cdf)) {
+                total = cdf;
+                count = data.length;
+                calc = true
+            }else{
+                calc = false
+            }
+
+
+    }
+
     const params = new URLSearchParams({
         where: whereClause,
-        outFields: "id_grid2,poc_3000",
+        outFields: "id_grid2,field_si_1,Total_1,def_1,UCSBACRES,Eligible",
         f: "json"
     });
 
@@ -25,42 +102,46 @@ async function calculateRisk() {
             return;
         }
 
-        // Calcular média da coluna poc_3000
-        let total = 0;
-        let count = 0;
+        if (data.features.length > 1){
+            console.log("maior que 1")
+            console.log(data.features.length)
+            processManyData(data)
+        }
+        if(calc == false){
+            console.log("igual a 1")
+            console.log(data.features.length)
+            processOneData(data)
+        }
 
-        data.features.forEach(feature => {
-            const value = feature.attributes.poc_3000 * 100;
-            if (value !== null && !isNaN(value)) {
-                total += value;
-                count++;
-            }
-        });
-
-        if (count === 0) {
-            document.getElementById("result").innerText = "No valid poc_3000 values found.";
+        console.log(total)
+        console.log(calc)
+        if (data.features.length === 0) {
+            document.getElementById("result").innerText = "No valid values found.";
             return;
         }
 
-        const average = total / count;
+        //average = total / count;
+        console.log(total)
+        var average = total
+        console.log("------")
 
         // Definir status com base na média
         let riskStatus = "";
 
-        if (average < 20) {
+        if (average < 0.2) {
             riskStatus = "Very Low";
-        } else if (average < 35) {
+        } else if (average < 0.35) {
             riskStatus = "Low";
-        } else if (average < 65) {
+        } else if (average < 0.65) {
             riskStatus = "Medium";
-        } else if (average < 80) {
+        } else if (average < 0.80) {
             riskStatus = "High";
         } else {
             riskStatus = "Very High";
         }
-
+        //average.toFixed(2)
         document.getElementById("result").innerHTML = `
-            <strong>Average poc_3000:</strong> ${average.toFixed(2)}%<br>
+            <strong>Prob:</strong> ${(average.toFixed(4) * 100)}%<br>
             <strong>Risk Status:</strong> ${riskStatus}
         `;
 
